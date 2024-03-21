5c1fbd4a372dbb31641a9e5702e8cdfeac500bd2
tiraxel <tiraxelsystems@gmail.com>
2024-03-21 15:59:23 -0300

    Update testgradle.sh
    Actualizacion post revisión

--- app/testgradle.sh
+++ app/testgradle.sh
@@ -4,5 +4,4 @@ NAV=$2
 
 # Ejecucion de prueba
 #gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV | tee logGradle.txt || true
-gradle build --refresh-dependencies
 xvfb-run -a --server-args="-screen 0 1920x1080x24" gradle runWithCucumber -Ptags=$TAG -Pnav=$NAV || true
